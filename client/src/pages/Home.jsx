3882a846f507599a9521c879c3646d91214e75bb
Joseph Jamolod <josephjamolod@gmail.com>
2024-05-24 22:03:11 +0800

    Fix home page responsiveness

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,31 +11,31 @@ export default function Home() {
       variants={fadeIn("down", 0.2)}
       initial="hidden"
       whileInView={"show"}
-      className="flex flex-col md:flex-row  justify-center 2xl:mt-20 lg:px-20 xl:px-40 2xl:px-60"
+      className="flex flex-col lg:flex-row gap-y-5 justify-center xl:mt-20 lg:px-20 xl:px-40 2xl:px-60"
     >
-      <div className="flex flex-col flex-1 px-5 items-center text-center md:items-start md:text-start md:pl-20 md:pr-0 xl:pl-40 2xl:pl-60 ">
+      <div className="flex flex-col flex-1 mt-10 px-5 items-center text-center lg:items-start lg:text-start lg:pl-20 lg:pr-0 xl:pl-40 2xl:pl-60 ">
         <p
           className={` text-black font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 `}
         >
           Hi there, I'm
         </p>
-        <h1 className="font-bold text-6xl lg:text-8xl">Joseph</h1>
-        <p className=" my-5 font-semibold max-w-80 text-base pt-5">
+        <h1 className="font-bold text-5xl md:text-6xl lg:text-8xl">Joseph</h1>
+        <p className=" my-5 font-semibold max-w-80 text-sm sm:text-base  pt-5">
           A full stack web developer specializing in creating dynamic and
           responsive web applications.
         </p>
-        <p className="text-base font-semibold max-w-80">
+        <p className="text-sm md:text-base font-semibold max-w-80">
           Explore my projects to see how I bring ideas to life.
         </p>
         <Link
           to="/projects"
-          className="text-white mt-5  px-5 bg-gray-900 min-h-10 md:min-h-14 flex items-center justify-center gap-2 hover:bg-white  hover:border hover:border-gray-900 hover:text-gray-900 transition duration-300"
+          className="text-white mt-5  px-5 bg-gray-900 min-h-10 lg:min-h-14 flex items-center justify-center gap-2 hover:bg-white  hover:border hover:border-gray-900 hover:text-gray-900 transition duration-300"
         >
           See Projects
           <MdOutlineKeyboardDoubleArrowRight className="text-2xl" />
         </Link>
       </div>
-      <div className="flex flex-1 px-5 md:px-0 justify-center 2xl:justify-start 2xl:flex-auto 2xl:pl-32">
+      <div className="flex flex-1 px-5 lg:px-0 justify-center 2xl:justify-start 2xl:flex-auto 2xl:pl-32">
         <img className="max-h-96" src={hero} alt="hero" />
       </div>
     </motion.div>
